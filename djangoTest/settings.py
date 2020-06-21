b2fc47c3b1a3b6071a7ae270093b83c80baa5ae7
Izael RG <izaelrg3@gmail.com>
2020-06-20 23:27:39 -0500

    Configurar setting para permitir phythonanywere

--- djangoTest/settings.py
+++ djangoTest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ui_3$tp-(!w#0%4z55=k+zaexp0v&md7ob1*273axph-n(!2j('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
