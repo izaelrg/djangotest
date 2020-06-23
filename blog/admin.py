c5e330ec3be5d4f64454f20d58f6823fe5b00d87
Izael RG <izaelrg3@gmail.com>
2020-06-23 15:55:20 -0500

    + Lista con Post no publicados + Agregar boton para publicar + Agregar boton para eliminar

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Post
 
+
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_date', 'author')
