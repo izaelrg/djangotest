a15ecbc2032e5a131f271176ec6ede5b87b9a183
Izael RG <izaelrg3@gmail.com>
2020-06-25 14:42:12 -0500

    Creación de App Polls con sus Models

--- djangoTest/settings.py
+++ djangoTest/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # Local
     'blog',
+    'polls',
 ]
 
 MIDDLEWARE = [
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es-mx'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Mexico_City'
 
 USE_I18N = True
 
