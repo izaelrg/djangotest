e018e32dfb3e6c752bf3978487befef15c01dd27
Izael RG <izaelrg3@gmail.com>
2020-06-27 14:31:01 -0500

    + POLLS edición en los modelos y vistas genericas + Se modificó Settings para leer la ruta templates + Se modifico base_site.html para mostrar otro titulo en el Admin

--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
+
 from . import views
 
 
 urlpatterns = [
-    path('polls/', views.polls, name='polls'),
-    path('polls/<int:question_id>/', views.detail, name='poll_detail'),
-    path('polls/<int:question_id>/results/', views.results, name='poll_results'),
+    path('polls/', views.PollView.as_view(), name='polls'),
+    path('polls/<int:pk>/', views.DetailView.as_view(), name='poll_detail'),
+    path('polls/<int:pk>/results/', views.ResultsView.as_view(), name='poll_results'),
     path('polls/<int:question_id>/vote/', views.vote, name='poll_vote'),
 ]
-
--- polls/views.py
+++ polls/views.py
@@ -1,25 +1,38 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.utils import timezone
 from django.urls import reverse
+from django.views import generic
 
-from .models import Question, Choice
-
-
-# Create your views here.
-def polls(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/poll_list.html', context)
+from .models import Choice, Question
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/poll_detail.html', {'question': question})
+class PollView(generic.ListView):
+    template_name = 'polls/poll_list.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/poll_result.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/poll_detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/poll_result.html'
 
 
 def vote(request, question_id):
